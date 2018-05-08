1498256b5ecce67714f0826d01c224828d66eba8
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-08 13:37:04 -0700

    making sure everythings up to date

--- src/configs/constants.js
+++ src/configs/constants.js
@@ -3,10 +3,9 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL:
-    process.env.DATABASE_URL || 'mongodb://localhost/musixchange',
-  TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL ||
+  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+  // process.env.DATABASE_URL || 'mongodb://localhost/musixchange',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL ||
     'mongodb://localhost/musixchange-test',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '15m',
